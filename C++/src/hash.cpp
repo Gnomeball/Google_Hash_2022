fb3150ac75851fd172d89664c31753dfa1c5f105
Gnomeball <gnome.c@protonmail.com>
2022-02-25 15:29:39 +0000

    Now reads input

--- C++/src/hash.cpp
+++ C++/src/hash.cpp
@@ -1,9 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include "contributor.hpp"
+#include "hash.hpp"
 #include "project.hpp"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+
+    // * Parse the args
+
+    if (argc == 1) {
+        printf("No file argument supplied, exiting.\n");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Opening file : %s\n", argv[1]);
+    }
+
+    // * Open the file
+
+    std::ifstream input_data;
+    input_data.open(argv[1]);
+
+    // * Read the data? please
+
+    int n_contributors;
+    int n_projects;
+    std::vector<Contributor> contributors;
+    std::vector<Project> projects;
+
+    readLimits(input_data, &n_contributors, &n_projects);
+    readContributors(input_data, n_contributors, contributors);
+    readProjects(input_data, n_projects, projects);
+
+    // * Output data, for reasons of "does it work?"
+
+    std::cout << "n_contributors = " << n_contributors << "\n";
+    std::cout << "n_contributors = " << n_projects << "\n";
+
+    for (auto c : contributors)
+        std::cout << c.getName() << "\n";
+
+    for (auto p : projects)
+        std::cout << p.getName() << "\n";
+
+    // * Close the file
+
+    input_data.close();
+
     return 0;
 }
--- C++/src/hash.hpp
+++ C++/src/hash.hpp
@@ -1,17 +1,83 @@
 #ifndef hash_h
 #define hash_h
 
-class Hash {
-    // Fields
+#include <fstream>
+#include <string>
+#include <vector>
 
-public:
-    // Constructor(s) / Destructor(s)
+#include "contributor.hpp"
+#include "project.hpp"
+#include "role.hpp"
+#include "skill.hpp"
 
-    // Member Functions
+inline void readLimits(std::ifstream &input_data, int *n_contributors, int *n_projects) {
+    std::string buffer;
+    std::getline(input_data, buffer);
 
-    // Utility Functions
+    std::string token = buffer.substr(0, buffer.find(" "));
+    *n_contributors = std::atoi(token.c_str());
+    token = buffer.substr(token.length(), buffer.back());
+    *n_projects = std::atoi(token.c_str());
+}
 
-    // Overrides
-};
+inline Contributor readContributor(std::ifstream &input_data) {
+    std::string buffer;
+    std::getline(input_data, buffer);
+
+    std::string token = buffer.substr(0, buffer.find(" "));
+    std::string name = token;
+    token = buffer.substr(token.length(), buffer.back());
+    int n_skills = std::atoi(token.c_str());
+
+    std::vector<Skill> skills;
+    for (int i = 0; i < n_skills; i++) {
+        std::getline(input_data, buffer);
+        token = buffer.substr(0, buffer.find(" "));
+        std::string name = token;
+        token = buffer.substr(token.length(), buffer.back());
+        int level = std::atoi(token.c_str());
+        skills.push_back(Skill(name, level));
+    }
+    return Contributor(name, skills);
+}
+
+inline void readContributors(std::ifstream &input_data, int n_contributors,
+                             std::vector<Contributor> &contributors) {
+    for (int i = 0; i < n_contributors; i++)
+        contributors.push_back(readContributor(input_data));
+}
+
+inline Project readProject(std::ifstream &input_data) {
+    std::string buffer;
+    std::getline(input_data, buffer);
+
+    std::string token = buffer.substr(0, buffer.find(" "));
+    std::string name = token;
+    token = buffer.substr(0, buffer.find(" "));
+    int days = std::atoi(token.c_str());
+    token = buffer.substr(0, buffer.find(" "));
+    int score = std::atoi(token.c_str());
+    token = buffer.substr(0, buffer.find(" "));
+    int dueBy = std::atoi(token.c_str());
+    token = buffer.substr(0, buffer.find(" "));
+    int n_roles = std::atoi(token.c_str());
+
+    std::vector<Role> roles;
+    for (int i = 0; i < n_roles; i++) {
+        std::getline(input_data, buffer);
+        token = buffer.substr(0, buffer.find(" "));
+        std::string name = token;
+        token = buffer.substr(token.length(), buffer.back());
+        int level = std::atoi(token.c_str());
+        roles.push_back(Role(name, level));
+    }
+    return Project(name, days, score, dueBy, roles);
+}
+
+inline void readProjects(std::ifstream &input_data, int n_projects,
+                         std::vector<Project> &projects) {
+    for (int i = 0; i < n_projects; i++)
+        projects.push_back(readProject(input_data));
+}
 
 #endif // hash_h
